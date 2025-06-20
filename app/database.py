062df1c8a2879266a5e6b4367163cb5aef9223a4
ksee1230 <ksee1230@gmail.com>
2025-06-20 16:57:23 +0900

    build, ci: 빌드 파일 수정, CI 설정 파일 수정

--- app/database.py
+++ app/database.py
@@ -5,15 +5,16 @@ from motor.motor_asyncio import AsyncIOMotorClient
 load_dotenv(find_dotenv(usecwd=True))
 
 # 기존 MongoDB URI 구성 방식 유지
-MONGO_URI = (
-    f"mongodb://{os.getenv('DB_USER')}:{os.getenv('MONGO_PASSWORD')}"
-    f"@{os.getenv('MONGO_URL')}:{os.getenv('MONGO_PORT')}"
-    f"/{os.getenv('MONGO_DB')}?authSource=admin"
-)
+user = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASSWORD')
+hosts = os.getenv('MONGO_HOSTS')
+db_name = os.getenv('MONGO_DB')
+replica_set = os.getenv('MONGO_REPLICA_SET')
 
-# 데이터베이스 이름도 환경 변수에서 가져옴 (기존 'MONGO_DB')
-DB_NAME = os.getenv('MONGO_DB') # 'product' 데이터베이스를 사용
+MONGO_URI = (
+    f"mongodb://{user}:{password}@{hosts}/{db_name}?authSource=admin&replicaSet={replica_set}"
+)
 
 # 동기 클라이언트 대신 비동기 클라이언트를 사용합니다.
 client = AsyncIOMotorClient(MONGO_URI)
-db = client[DB_NAME]
+db = client[db_name]
